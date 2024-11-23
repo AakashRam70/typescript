b9123fb56890f52853d2a0f731a5c1abc3ec7ce6
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-11-23 16:23:39 +0530

    adding sum.ts

---
+++ node-app/sum.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+let val = sum(3, 2);
+console.log(val);
---
+++ node-app/sum.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number):number {
+    return a + b;
+}
+
+let val = sum(3, 2);
+console.log(val);
